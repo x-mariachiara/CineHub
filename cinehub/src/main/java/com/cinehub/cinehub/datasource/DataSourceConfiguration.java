c97bc4a30350361734f865abfa8825d349e30c34
peppe-99 <g.cardaropoli15@studenti.unisa.it>
2020-10-14 18:03:07 +0200

    Inizializzazione progetto springboot

---
+++ cinehub/src/main/java/com/cinehub/cinehub/datasource/DataSourceConfiguration.java
@@ -0,0 +1,19 @@
+package com.cinehub.cinehub.datasource;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class DataSourceConfiguration {
+
+    @Bean
+    @ConfigurationProperties("app.datasource")
+    public HikariDataSource hikariDataSource(){
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+
+}
